f4c98c0e46c2e6fd0b23f4d3256f7d466bbde576
Smarpit Singh <smarpitsingh560@gmail.com>
2018-07-15 23:17:06 +0530

    EditTextPreference Constraints

--- app/src/main/java/com/example/android/visualizer_test/SettingsFragment.java
+++ app/src/main/java/com/example/android/visualizer_test/SettingsFragment.java
@@ -8,14 +8,17 @@ import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements
-        SharedPreferences.OnSharedPreferenceChangeListener {
+        SharedPreferences.OnSharedPreferenceChangeListener , Preference.OnPreferenceChangeListener{
+
+
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
 
-
+        // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
         addPreferencesFromResource(R.xml.pref_visualizer);
 
         SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
@@ -32,10 +35,13 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
                 setPreferenceSummary(p, value);
             }
         }
+
+
+        // Add the preference listener which checks that the size is correct to the size preference
+        Preference preference = findPreference(getString(R.string.pref_size_key));
+        preference.setOnPreferenceChangeListener(this);
     }
 
-
-    // call setPreferenceSummary on the changed preference
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         // Figure out which preference was changed
@@ -50,11 +56,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
     }
 
 
-    // This method should check if the preference is a ListPreference and, if so, find the label
-    // associated with the value. You can do this by using the findIndexOfValue and getEntries methods
-    // of Preference.
     private void setPreferenceSummary(Preference preference, String value) {
-        // COMPLETED (3) Don't forget to add code here to properly set the summary for an EditTextPreference
         if (preference instanceof ListPreference) {
             // For list preferences, figure out the label of the selected value
             ListPreference listPreference = (ListPreference) preference;
@@ -67,6 +69,34 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             // For EditTextPreferences, set the summary to the value's simple string representation.
             preference.setSummary(value);
         }
+    }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        // In this context, we're using the onPreferenceChange listener for checking whether the
+        // size setting was set to a valid value.
+
+        Toast error = Toast.makeText(getContext(), "Please select a number between 0.1 and 3", Toast.LENGTH_SHORT);
+
+        // Double check that the preference is the size preference
+        String sizeKey = getString(R.string.pref_size_key);
+        if (preference.getKey().equals(sizeKey)) {
+            String stringSize = (String) newValue;
+            try {
+                float size = Float.parseFloat(stringSize);
+                // If the number is outside of the acceptable range, show an error.
+                if (size > 3 || size <= 0) {
+                    error.show();
+                    return false;
+                }
+            } catch (NumberFormatException nfe) {
+                // If whatever the user entered can't be parsed to a number, show an error
+                error.show();
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
